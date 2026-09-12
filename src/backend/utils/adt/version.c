cfa45bb003924e38f1968aa5dfbbf47ff647ff6d
IseN <lkyhfx3468@163.com>
2026-09-12 23:09:20 +0800

    Customize version() for PolarDB compatibility

--- src/backend/utils/adt/version.c
+++ src/backend/utils/adt/version.c
@@ -17,8 +17,21 @@
 #include "utils/builtins.h"
 
 
+#if defined(__x86_64__) || defined(_M_X64)
+#define POLARDB_COMPAT_ARCH "x86_64"
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define POLARDB_COMPAT_ARCH "aarch64"
+#else
+#define POLARDB_COMPAT_ARCH "unknown"
+#endif
+
+#define POLARDB_COMPAT_VERSION_STRING \
+	"PostgreSQL 17.9 (PolarDB-compatible 17.9.1.0 build unknown) on " \
+	POLARDB_COMPAT_ARCH "-linux-gnu"
+
+
 Datum
 pgsql_version(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_TEXT_P(cstring_to_text(PG_VERSION_STR));
+	PG_RETURN_TEXT_P(cstring_to_text(POLARDB_COMPAT_VERSION_STRING));
 }
